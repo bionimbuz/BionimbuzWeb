3a8e22963d68e6c18b2e6375f280f8ef4b769b94
Felipe L. de Souza <felipelopess@gmail.com>
2017-10-19 18:46:42 -0200

    Error preparing java installation

--- prepare.sh
+++ prepare.sh
@@ -52,13 +52,10 @@ if [ ! -z "$PREPARE_ALL" ]; then
     echo "# Installing dependencies"
     echo "# ======================================="
 
-    sudo add-apt-repository ppa:webupd8team/java -y
-    sudo apt-get update
-
     sudo apt-get install -y \
-        oracle-java8-installer \
         postgresql postgresql-client \
-        maven
+        maven \
+	openjdk-8-jdk
 
     echo "# Downloading Playframework"
     echo "# ======================================="
