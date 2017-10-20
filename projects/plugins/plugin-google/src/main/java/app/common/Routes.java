d5c168af82520d7680c080fc2ff7e5a3f753430d
Felipe Lopes de Souza <felipe.souza@brisabr.com.br>
2017-10-20 16:59:39 -0200

    Added info controller test

---
+++ projects/plugins/plugin-google/src/main/java/app/common/Routes.java
@@ -0,0 +1,5 @@
+package app.common;
+
+public class Routes {
+    public static final String INFO = "/info";
+}
---
+++ projects/plugins/plugin-google/src/test/java/app/controllers/InfoControllerTest.java
@@ -0,0 +1,53 @@
+package app.controllers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import app.common.Response;
+import app.common.Routes;
+import app.common.SystemConstants;
+import app.models.InfoModel;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class InfoControllerTest {
+
+    @Autowired
+    private InfoController controller;
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @Value("${local.server.port}")
+    private int PORT;
+
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+
+    @Test
+    public void infoTest() {
+
+        Response<InfoModel> model = this.restTemplate
+                .exchange(
+                        Routes.INFO, 
+                        HttpMethod.GET, 
+                        null,
+                        new ParameterizedTypeReference<Response<InfoModel>>() {})
+                .getBody();
+
+        assertThat(model.getContent()).isNotNull();
+        assertThat(model.getContent().getVersion())
+                .isEqualTo(SystemConstants.SYSTEM_VERSION);
+    }
+
+}
