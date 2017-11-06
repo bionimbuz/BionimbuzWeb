a03fca8a61f5762e997917ac79f79979be8a045b
Felipe L. de Souza <felipelopess@gmail.com>
2017-11-06 21:04:45 -0200

    OAuth separated from Google API jclouds

--- projects/plugins/plugin-google/src/main/java/app/common/GoogleComputeEngineUtils.java
+++ projects/plugins/plugin-google/src/main/java/app/common/GoogleComputeEngineUtils.java
@@ -1,49 +1,103 @@
 package app.common;
 
+import static com.google.common.util.concurrent.MoreExecutors.newDirectExecutorService;
+
+import java.io.IOException;
 import java.net.URI;
+import java.net.URL;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Properties;
 
 import org.jclouds.ContextBuilder;
 import org.jclouds.compute.ComputeServiceContext;
+import org.jclouds.concurrent.config.ExecutorServiceModule;
 import org.jclouds.domain.Credentials;
 import org.jclouds.googlecloud.GoogleCredentialsFromJson;
 import org.jclouds.googlecomputeengine.GoogleComputeEngineApi;
-import org.jclouds.googlecomputeengine.GoogleComputeEngineProviderMetadata;
+import org.jclouds.googlecomputeengine.GoogleComputeEngineApiMetadata;
 import org.jclouds.googlecomputeengine.domain.Operation;
 import org.jclouds.googlecomputeengine.features.NetworkApi;
 import org.jclouds.googlecomputeengine.features.OperationApi;
+import org.jclouds.oauth.v2.AuthorizationApi;
 import org.jclouds.oauth.v2.config.CredentialType;
+import org.jclouds.oauth.v2.config.OAuthModule;
 import org.jclouds.oauth.v2.config.OAuthProperties;
+import org.jclouds.oauth.v2.config.OAuthScopes;
+import org.jclouds.oauth.v2.config.OAuthScopes.SingleScope;
+import org.jclouds.oauth.v2.domain.Claims;
+import org.jclouds.oauth.v2.domain.Token;
+import org.jclouds.rest.AnonymousHttpApiMetadata;
 
 import com.google.common.base.Supplier;
-import com.google.inject.Injector;
+import com.google.common.collect.ImmutableSet;
+import com.google.inject.Binder;
+import com.google.inject.Module;
 
 public class GoogleComputeEngineUtils {    
     
+    private static final String READ_SCOPE = "https://www.googleapis.com/auth/compute.readonly"; 
+    private static final String WRITE_SCOPE = "https://www.googleapis.com/auth/compute"; 
+    
     public static GoogleComputeEngineApi createApi(final String credential) {
-        try {
-            Supplier<Credentials> credentialSupplier = new GoogleCredentialsFromJson(
-                    credential);
-
-            ComputeServiceContext context = ContextBuilder
-                    .newBuilder(SystemConstants.CLOUD_TYPE)
-                    .credentialsSupplier(credentialSupplier)
-                    .buildView(ComputeServiceContext.class);
-
-            Credentials credentials = credentialSupplier.get();
-            ContextBuilder contextBuilder = ContextBuilder
-                    .newBuilder(GoogleComputeEngineProviderMetadata.builder()
-                            .build())
-                    .credentials(credentials.identity, credentials.credential);
-
-            Injector injector = contextBuilder.buildInjector();
-            return injector.getInstance(GoogleComputeEngineApi.class);
-            
+        try {                        
+            Supplier<Credentials> credentialSupplier = 
+                    new GoogleCredentialsFromJson(credential);
+            Credentials credentials = credentialSupplier.get();            
+            Token token = getToken(credentials);              
+            return createApiFromToken(credentials.identity, token.accessToken());            
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
             return null;
         }
+    }
+       
+    private static Token getToken(Credentials credentials) {
+        
+        try {
+            int now = (int)ZonedDateTime.now(ZoneOffset.UTC).toEpochSecond();
+            
+            Claims claims = Claims.create( //
+                    credentials.identity, // iss
+                    WRITE_SCOPE, // scope
+                    "https://accounts.google.com/o/oauth2/token", // aud
+                    now + 60*60, // placeholder exp for the cache
+                    now // placeholder iat for the cache
+                );
+            
+            AuthorizationApi api = api(
+                                    credentials, 
+                                    new URL("https://accounts.google.com/o/oauth2/v2/auth"));
+
+            Token token = api.authorize(claims);
+            return token;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    private static AuthorizationApi api(Credentials credentials, URL url)
+            throws IOException {
+
+        return ContextBuilder
+                .newBuilder(
+                        AnonymousHttpApiMetadata.forApi(AuthorizationApi.class))
+                .credentials(credentials.identity, credentials.credential)
+                .endpoint(url.toString())
+                .overrides(GoogleComputeEngineApiMetadata.defaultProperties())
+                .modules(ImmutableSet.of(
+                        new ExecutorServiceModule(newDirectExecutorService()),
+                        new OAuthModule(), new Module() {
+                            @Override
+                            public void configure(Binder binder) {
+                                binder.bind(OAuthScopes.class).toInstance(
+                                        SingleScope.create(WRITE_SCOPE));
+                            }
+                        }))
+                .buildApi(AuthorizationApi.class);
     }
     
     public static GoogleComputeEngineApi createApiFromToken(final String identity, final String token) {
