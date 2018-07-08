7a4aafb3a9fa08c3df7341c4ec858a7e9bdf6e49
Felipe L. de Souza <felipelopess@gmail.com>
2018-07-08 19:31:30 -0300

    Execution test

--- projects/coordinators/executor-coordinator/src/test/java/app/controllers/ExecutionControllerTest.java
+++ projects/coordinators/executor-coordinator/src/test/java/app/controllers/ExecutionControllerTest.java
@@ -3,6 +3,9 @@ package app.controllers;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,11 +18,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 import app.client.ExecutionApi;
 import app.models.Body;
 import app.models.Command;
+import app.models.RemoteFileInfo;
 import utils.TestUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ExecutionControllerTest {
+
+    private static final String PLUGIN_LOCAL_DIR = "../../plugins/plugin-local/";
+    private static final String SPACES_DIR = PLUGIN_LOCAL_DIR + "spaces/";
+    private static final String INSTANCES_DIR = PLUGIN_LOCAL_DIR + "instances/";
 
     @Autowired
     private ExecutionController controller;
@@ -34,19 +42,41 @@ public class ExecutionControllerTest {
 	@Test
 	public void startExecutionTest() throws IOException {
 
-	    Command command = new Command();
-
-	    command.setWorkinDir("../../plugins/plugin-local/instances/test");
-	    command.setCommandLine("test_script.sh");
-
-
-
         ExecutionApi api = new ExecutionApi(TestUtils.getUrl(PORT));
+
+	    List<RemoteFileInfo> listInputFiles =
+	            new ArrayList<>();
+        listInputFiles.add(
+                createRemoteFileInfo("GET", "test", "test_input1.txt"));
+        listInputFiles.add(
+                createRemoteFileInfo("GET", "test", "test_input2.txt"));
+
+        List<String> listOutputFiles = new ArrayList<>();
+        listOutputFiles.add("test_output.txt");
+
+	    Command command = new Command();
+	    command.setWorkinDir(INSTANCES_DIR + "bionimbuz-instance-0/");
+	    command.setCommandLine("test_script.sh {i:1} {i:2} {o:1}");
+        command.setListInputs(listInputFiles);
+        command.setListOutputs(listOutputFiles);
+        command.setArgs("-a -b -c content");
 
         Body<Boolean> body = api.postCommand(command);
 
-
-
-	    assertThat(true).isTrue();
+	    assertThat(body).isNotNull();
+	    assertThat(body.getContent()).isTrue();
 	}
+
+    private RemoteFileInfo createRemoteFileInfo(
+            final String method,
+            final String space,
+            final String fileName) {
+        RemoteFileInfo inputFile;
+        inputFile = new RemoteFileInfo();
+	    inputFile.setMethod(method);
+	    inputFile.setName(fileName);
+	    inputFile.setHeaders(new HashMap<String, String>());
+	    inputFile.setUrl("http://localhost:8282/spaces/"+space+"/file/"+fileName+"/download");
+	    return inputFile;
+    }
 }
