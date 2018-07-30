9ea4dc5a4442d1d4935a44b2b2733b54c1834d6e
Jefferson Chaves Gomes <jefferson.gomes@brisa.org.br>
2018-07-30 12:32:33 -0300

    Adding fault injection thread

--- projects/web/app/jobs/FaultToleranceConfigurationJob.java
+++ projects/web/app/jobs/FaultToleranceConfigurationJob.java
@@ -4,32 +4,40 @@ import static app.commons.constants.TimeConstants.DEFAULT_TIME_UNIT;
 
 import java.lang.management.ManagementFactory;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 import app.FaultToleranceModule;
 import app.commons.exceptions.SystemException;
+import app.commons.utils.LoggerUtil;
 import app.models.AttemptsNumber;
 import app.models.Level;
 import app.models.Retry;
 import app.models.SoftwareRejuvenation;
 import app.models.Timeout;
 import app.services.BootstrapService;
-import play.Logger;
 import play.jobs.Job;
+import play.jobs.OnApplicationStart;
 
-// @OnApplicationStart
+@OnApplicationStart
 public class FaultToleranceConfigurationJob extends Job {
 
     private static final String RUNTIME_MODULE_ID = ManagementFactory.getRuntimeMXBean().getName();
     private final static String STARTUP_COMMAND = "/Volumes/Data/developer/servers/play-1.4.4/play-1.4.4 start " + System.getProperty("user.dir");
-    private static final String STARTUP_COORDINATOR_COMMAND = "java -jar ./conf/fault-tolerance/ft-coordinator-0.0.1-exec.jar";
+    private static final String STARTUP_COORDINATOR_COMMAND = "java -jar ./conf/fault-tolerance/ft-coordinator-0.0.1-exec.jar --server.port=7775";
     private static final String FAILED_TO_START_FAULT_TOLERANCE_MODULE = "Failed to start FaultToleranceModule!";
-    private static final long HEARTBEAT_TIME = 3L;
+    private static final long HEARTBEAT_TIME = 1L;
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // * @see play.jobs.Job#doJob()
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     @Override
     public void doJob() {
+
+        startFaultToleranceModule();
+        new FaultInjectionMTBFThread().start();
+    }
+
+    private static void startFaultToleranceModule() {
 
         CompletableFuture.runAsync(() -> {
             try {
@@ -40,7 +48,7 @@ public class FaultToleranceConfigurationJob extends Job {
                 faultToleranceLevel.addTechnique(bindRetry());
                 faultToleranceModule.start(faultToleranceLevel, STARTUP_COORDINATOR_COMMAND);
             } catch (SystemException | InterruptedException e) {
-                Logger.error(e, FAILED_TO_START_FAULT_TOLERANCE_MODULE);
+                LoggerUtil.error(FAILED_TO_START_FAULT_TOLERANCE_MODULE, e);
             }
         });
     }
@@ -56,6 +64,28 @@ public class FaultToleranceConfigurationJob extends Job {
 
     private static Retry bindRetry() {
         final Timeout timeout = new Timeout(1L, DEFAULT_TIME_UNIT);
-        return new Retry(timeout, new AttemptsNumber());
+        return new Retry(timeout, new AttemptsNumber(1L));
+    }
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Inner classes to fault injection
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    private static class FaultInjectionMTBFThread extends Thread {
+
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // * @see java.lang.Thread#run()
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        @Override
+        public void run() {
+
+            try {
+                LoggerUtil.warn("Waiting to fault injection");
+                TimeUnit.MINUTES.sleep(5);
+                LoggerUtil.warn("Doing fault injection");
+                System.exit(1);
+            } catch (final InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
